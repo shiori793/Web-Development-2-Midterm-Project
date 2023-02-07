256f1e3fe9651bf10e29797db433d55bd1b0ab3c
Shiori <shiori.fujii.cb@gmail.com>
2023-02-07 15:29:28 -0800

    finish project

--- app-signin.js
+++ app-signin.js
@@ -1,23 +1,24 @@
-function login() {
+const loginButton = document.getElementById('login-button');
+const signinButton = document.getElementById('signin-button');
+
+loginButton.addEventListener('click', () => {
     const loginInput = document.getElementById('floatingUserId').value;
     if (checkStorage(loginInput)) {
         user_id = loginInput;
         sessionStorage.setItem('loginUserID', user_id);
-        window.location.href = './home.html';
     } else {
         alert('Your User ID is not valid. Please try again.');
     };
-};
+});
 
-function signin() {
+signinButton.addEventListener('click', () => {
     user_id = localStorage.length + 1;
     sessionStorage.setItem('loginUserID', user_id);
-    window.location.href = './home.html';
-}
+});
 
 function checkStorage(input) {
-    for (let i = 0; localStorage.length; i++) {
-        if (localStorage.key[i] == input) {
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) == input) {
             return true;
         }
     }
--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const clearButton = document.getElementById("clear-button");
 const checkoutButton = document.getElementById("checkout-button");
 const cartTotal = document.getElementById('cart-total');
 const numberOfTotalItem = document.getElementById('number-of-total-item');
+const signOutButton = document.getElementById('signout-button');
+const navUserId = document.getElementById('nav-userId');
 
-let user_id = 0;;
+let user_id = 0;
+let cart = null;
 
 class Cart {
     constructor(user_id) {
@@ -19,29 +22,28 @@ class Cart {
         if (this.cartItems.some(value => value.id == product.id)) {
             let index = this.cartItems.findIndex(value => value.id == product.id);
             this.cartItems[index].quantity += 1;
-            localStorage.setItem(this.user_id, this.cartItems);
+            localStorage.setItem(this.user_id, JSON.stringify(this.cartItems));
             return true; //item already exists
         } else {
             this.cartItems.push({
                 id: product.id,
+                title: product.title,
                 price: product.price,
                 quantity: 1
             })
-            localStorage.setItem(this.user_id, this.cartItems);
+            localStorage.setItem(this.user_id, JSON.stringify(this.cartItems));
             return false; //add new item
         }
     }
 
     deleteFromCart(product) {
         this.cartItems = this.cartItems.filter(value => value.id != product.id);
-        localStorage.setItem(this.user_id, this.cartItems);
-        return this.cartItems;
+        localStorage.setItem(this.user_id, JSON.stringify(this.cartItems));
     }
 
     clearCart() {
         this.cartItems = [];
-        localStorage.setItem(this.user_id, this.cartItems);
-        return this.cartItems;
+        localStorage.setItem(this.user_id, JSON.stringify(this.cartItems));
     }
 
     getQuantity(product) {
@@ -60,6 +62,47 @@ class Cart {
     getTotalPrice() {
         return this.cartItems.reduce((total, value) => total += (value.price * value.quantity), 0);
     }
+}
+
+function addNewLineInCart(cart, item) {
+    const cartItem = document.createElement('li');
+    cartItem.classList.add('d-flex', 'dropdown-item', 'cart-item');
+    cartItem.id = item.id;
+
+    const cartItemImage = document.createElement('img');
+    cartItemImage.src = item.image;
+                        
+    const cartItemName = document.createElement('div');
+    cartItemName.classList.add('cart-item-name', 'dropdown-item');
+    cartItemName.id = 'cart-item-name';
+    const cartItemProduct = document.createElement('span');
+    cartItemProduct.classList.add('cart-item-product');
+    cartItemProduct.innerText = item.title;
+    const br = document.createElement('br');
+    const cartItemPrice = document.createElement('span');
+    cartItemPrice.classList.add('cart-item-price');
+    cartItemProduct.id = 'cart-item-price';
+    cartItemPrice.innerText = '$' + item.price + ' x ' + cart.getQuantity(item);
+    cartItemName.appendChild(cartItemProduct);
+    cartItemName.appendChild(br);
+    cartItemName.appendChild(cartItemPrice);
+        
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('dropdown-item', 'cart-item-delete');
+    const deleteButtonIcon = document.createElement('i');
+    deleteButtonIcon.classList.add('fa-solid', 'fa-trash');
+    deleteButton.appendChild(deleteButtonIcon);
+        
+    cartItem.appendChild(cartItemImage);
+    cartItem.appendChild(cartItemName);
+    cartItem.appendChild(deleteButton);
+    cartDropdownList.insertBefore(cartItem, document.getElementById('dropdown-divider'));
+        
+    deleteButton.addEventListener('click', () => {
+        cart.deleteFromCart(item);
+        cartDropdownList.removeChild(cartItem);
+        updateCartInfo(cart);
+    });
 }
 
 function createProductCards(list, cart) {
@@ -97,44 +140,7 @@ function createProductCards(list, cart) {
                     const targetItem = document.getElementById(item.id);
                     targetItem.childNodes[1].childNodes[2].innerText = '$' + item.price + ' x ' + cart.getQuantity(item);
                 } else {
-                        const cartItem = document.createElement('li');
-                        cartItem.classList.add('d-flex', 'dropdown-item', 'cart-item');
-                        cartItem.id = item.id;
-                        
-                        const cartItemImage = document.createElement('img');
-                        cartItemImage.src = item.image;
-                        
-                        const cartItemName = document.createElement('div');
-                        cartItemName.classList.add('cart-item-name', 'dropdown-item');
-                        cartItemName.id = 'cart-item-name';
-                        const cartItemProduct = document.createElement('span');
-                        cartItemProduct.classList.add('cart-item-product');
-                        cartItemProduct.innerText = item.title;
-                        const br = document.createElement('br');
-                        const cartItemPrice = document.createElement('span');
-                        cartItemPrice.classList.add('cart-item-price');
-                        cartItemProduct.id = 'cart-item-price';
-                        cartItemPrice.innerText = '$' + item.price + ' x ' + cart.getQuantity(item);
-                        cartItemName.appendChild(cartItemProduct);
-                        cartItemName.appendChild(br);
-                        cartItemName.appendChild(cartItemPrice);
-        
-                        const deleteButton = document.createElement('button');
-                        deleteButton.classList.add('dropdown-item', 'cart-item-delete');
-                        const deleteButtonIcon = document.createElement('i');
-                        deleteButtonIcon.classList.add('fa-solid', 'fa-trash');
-                        deleteButton.appendChild(deleteButtonIcon);
-        
-                        cartItem.appendChild(cartItemImage);
-                        cartItem.appendChild(cartItemName);
-                        cartItem.appendChild(deleteButton);
-                        cartDropdownList.insertBefore(cartItem, document.getElementById('dropdown-divider'));
-        
-                        deleteButton.addEventListener('click', () => {
-                            cart.deleteFromCart(item);
-                            cartDropdownList.removeChild(cartItem);
-                            updateCartInfo(cart);
-                        });
+                        addNewLineInCart(cart, item);
                     };
                     updateCartInfo(cart);
                 };
@@ -149,30 +155,26 @@ function updateCartInfo(cart) {
 
 window.addEventListener('load', () => {
     user_id = sessionStorage.getItem('loginUserID');
+    navUserId.innerText += ' ' + user_id;
     if (user_id != null && user_id > 0) {
-        const cart = new Cart(user_id);
-
-        clearButton.addEventListener('click', () => {
-            cart.clearCart();
-            const cartAllItems = document.querySelectorAll('.cart-item');
-            cartAllItems.forEach(item => {
-                cartDropdownList.removeChild(item);
-            });
-            updateCartInfo(cart);
-            localStorage.setItem(user_id, cart);
-        })
-
-        checkoutButton.addEventListener('click', () => {
-            user_id = 0;
-            sessionStorage.removeItem('loginUserID');
-            window.location.replace('./index.html');
-        });
-
         //1. get the product list from API and set all value to the card and append to html
         fetch("https://fakestoreapi.com/products/category/electronics")
             .then(res => res.json())
             .then(data => {
+                if (localStorage.getItem(user_id) != null) {
+                    let storageData = JSON.parse(localStorage.getItem(user_id));
+                    cart = new Cart(user_id);
+                    cart.cartItems = storageData;
+                } else {
+                    cart = new Cart(user_id);
+                }
+                
                 const productList = data;
+                cart.cartItems.forEach(elem => {
+                    let item = productList.find(value => value.id == elem.id);
+                    addNewLineInCart(cart, item);
+                });
+                updateCartInfo(cart);
                 createProductCards(productList, cart);
 
                 searchButton.addEventListener('click', () => {
@@ -180,6 +182,32 @@ window.addEventListener('load', () => {
                     const regex = new RegExp (searchInput.value, 'i');
                     resultList = productList.filter(item => regex.test(item.title));
                     createProductCards(resultList, cart);
+                });
+
+                clearButton.addEventListener('click', () => {
+                    cart.clearCart();
+                    const cartAllItems = document.querySelectorAll('.cart-item');
+                    cartAllItems.forEach(item => {
+                        cartDropdownList.removeChild(item);
+                    });
+                    updateCartInfo(cart);
+                    localStorage.setItem(this.user_id, JSON.stringify(this.cartItems));
+                })
+                
+                checkoutButton.addEventListener('click', () => {
+                    cart.clearCart();
+                    const cartAllItems = document.querySelectorAll('.cart-item');
+                    cartAllItems.forEach(item => {
+                        cartDropdownList.removeChild(item);
+                    });
+                    updateCartInfo(cart);
+                    localStorage.setItem(this.user_id, JSON.stringify(this.cartItems));
+                })
+                
+                signOutButton.addEventListener('click', () => {
+                    user_id = 0;
+                    sessionStorage.removeItem('loginUserID');
+                    window.location.replace('./index.html');
                 });
             });
     } else {
